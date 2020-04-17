02ad6360aee4e3218f73f1f9d2f02090a19070dd
Aaron Ferrucci <aaron.ferrucci@joby.aero>
2020-04-16 21:19:19 -0700

    daily and cumulative county plots

--- counties.R
+++ counties.R
@@ -3,69 +3,89 @@ library(ggplot2)
 library(dplyr)
 
 source("plotit.R")
-county_plot <- function(counties, countyname) {
+county_plot <- function(counties, countyname, type) {
   county <- counties[counties$county %in% countyname,]
-  p <- plotit(county, paste0(countyname, " results"))
+  p <- plotit(county, paste0(countyname, " ", type, " results"))
   return(p)  
 }
 
-plot_a_county <- function(counties, countyname) {
-  p <- county_plot(counties, countyname)
-  
-  png(filename=paste0("images/", countyname, "_test_results.png"), width=1264, height=673)
+plot_a_county <- function(counties, countyname, type) {
+  p <- county_plot(counties, countyname, type)
+  filename <- paste0("images/", gsub(" ", "_", countyname, fixed=T), "_", type, "_test_results.png")
+  png(filename=filename, width=1264, height=673)
   print(p)
   dev.off()
   return(p)
 }
 
+get_data <- function(raw, county_names, cumulative=T) {
+  
+  # some issues with the input data:
+  # column names are R-unfriendly, e.g. "Santa Clara +Tests", "Santa Clara -Tests"
+  # data is columnar, but I want row-wise
+  # plan: get the santa clara data into a usable format for plotting, then think about generalizing
+  # messy: relying on the mangled column names, e.g.
+  # "Santa Clara +Tests" became "Santa.Clara..Tests"
+  # "Santa Clara -Tests" became "Santa.Clara..Tests.1"
+  counties <- data.frame(
+    date=as.Date(character()),
+    county=character(),
+    result=character(),
+    count=integer(),
+    stringsAsFactors=F
+  )
+  for (county in county_names) {
+    colprefix <- gsub(" ", ".", county, fixed=T)
+    
+    # process positive test column
+    poscol <- paste0(colprefix, "..Tests")
+    
+    date <- as.Date(raw[,1])
+    count <- pull(raw, poscol)
+    count <- ifelse(is.na(count), 0, count)
+    
+    pos <- data.frame(
+      date=date,
+      county=county,
+      result="positive",
+      count=count
+    )
+    if (cumulative) {
+      pos$count <- cumsum(pos$count)
+    }
+  
+    counties <- rbind(counties, pos)
+    
+    # process negative  test column
+    negcol <- paste0(colprefix, "..Tests.1")
+    count <- pull(raw, negcol)
+    count <- ifelse(is.na(count), 0, count)
+    
+    neg <- data.frame(
+      date=date,
+      county=county,
+      result="negative",
+      count=count
+    )
+    if (cumulative) {
+      neg$count <- cumsum(neg$count)
+    }
+
+    counties <- rbind(counties, neg)
+  }
+  return(counties)  
+}
+
+# these are the counties with enough data for plotting:
+county_names <- c("San Francisco", "San Mateo", "Santa Clara")
+
 raw <- read.csv("covid_test_data.csv")
+counties <- get_data(raw, county_names, cumulative=T)
+for (county_name in county_names) {
+  p <- plot_a_county(counties, county_name, "cumulative")
+}
 
-# raw$Santa.Clara..Tests <- ifelse(is.na(raw$Santa.Clara..Tests), 0, raw$Santa.Clara..Tests)
-# raw$Santa.Clara..Tests.1 <- ifelse(is.na(raw$Santa.Clara..Tests.1), 0, raw$Santa.Clara..Tests.1)
-
-# some issues with the input data:
-# column names are R-unfriendly, e.g. "Santa Clara +Tests", "Santa Clara -Tests"
-# data is columnar, but I want row-wise
-# plan: get the santa clara data into a usable format for plotting, then think about generalizing
-# messy: relying on the mangled column names, e.g.
-# "Santa Clara +Tests" became "Santa.Clara..Tests"
-# "Santa Clara -Tests" became "Santa.Clara..Tests.1"
-counties <- data.frame(
-  date=as.Date(character()),
-  county=character(),
-  result=character(),
-  count=integer(),
-  stringsAsFactors=F
-)
-
-county <- "Santa Clara"
-colprefix <- gsub(" ", ".", county, fixed=T)
-poscol <- paste0(colprefix, "..Tests")
-negcol <- paste0(colprefix, "..Tests.1")
-
-date <- as.Date(raw[,1])
-count <- pull(raw, poscol)
-count <- ifelse(is.na(count), 0, count)
-
-pos <- data.frame(
-  date=date,
-  county=county,
-  result="positive",
-  count=count
-)
-pos$count <- cumsum(pos$count)
-
-count <- pull(raw, negcol)
-count <- ifelse(is.na(count), 0, count)
-
-neg <- data.frame(
-  date=date,
-  county=county,
-  result="negative",
-  count=count
-)
-neg$count <- cumsum(neg$count)
-
-counties <- rbind(pos, neg)
-
-p <- plot_a_county(counties, county)
+counties <- get_data(raw, county_names, cumulative=F)
+for (county_name in county_names) {
+  p <- plot_a_county(counties, county_name, "daily")
+}
