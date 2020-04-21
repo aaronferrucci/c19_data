dc7f22d72468e332395b8ed49a91373c1ca06bf7
Aaron Ferrucci <aaron.ferrucci@joby.aero>
2020-04-20 21:54:00 -0700

    Convert from monolithic csv to per-county csv

--- counties.R
+++ counties.R
@@ -34,15 +34,61 @@ tweak_data <- function(raw, cumulative=T) {
   return(counties)  
 }
 
+csv_filename <- function(county_name) {
+  filename <- paste0("csv/", gsub(" ", "_", county_name), ".csv")
+  return(filename)  
+}
+
+write_csv <- function(raw, county_name) {
+  county <- raw[raw$county == county_name, ]
+  # ascending by date
+  county <- county[order(county$date),]
+
+  filename <- csv_filename(county_name)
+  
+  # 3 blank lines at the top
+  cat("\n\n\n", file=filename)
+  # There's a warning for the append, but it seems to work.
+  write.table(county, filename, append=T, sep=",", row.names=F)
+}
+
+update_legacy_data <- function(county_names) {
+  raw <- read.table("covid_test_data.csv", header=T, sep=",", row.names=1)
+  
+  for (county_name in county_names) {
+    write_csv(raw, county_name)
+  }
+}
+
+get_data <- function(county_names) {
+  raw = NULL
+  for (county_name in county_names) {
+    county <- read.table(csv_filename(county_name), header=T, sep=",", skip=3)
+    
+    if (is.null(raw)) {
+      raw <- county
+    } else {
+      raw <- rbind(raw, county)
+    }
+  }
+  # type conversions, factor ordering
+  raw$date <- as.Date(raw$date)
+  # order the result factor... this may depend on alpha order, or on the way the data is constructed.
+  # "positive" should be first in the level order to produce stacked barplots with positive at the bottom.
+  if (levels(raw$result)[1] == "negative") {
+    raw$result <- factor(raw$result, levels(raw$result)[c(2:1)])
+  }
+  return(raw)
+}
+
 # these are the counties with enough data for plotting:
 county_names <- c("San Francisco", "San Mateo", "Santa Clara")
 
-raw <- read.csv("covid_test_data.csv")
-# order the result factor... this may depend on alpha order, or on the way the data is constructed.
-# "positive" should be first in the level order to produce stacked barplots with positive at the bottom.
-if (levels(raw$result)[1] == "negative") {
-  raw$result <- factor(raw$result, levels(raw$result)[c(2:1)])
-}
+# update_legacy_data(county_names)
+
+# reconstruct the "raw" data from per-county csv files
+raw <- get_data(county_names)
+
 counties <- tweak_data(raw, cumulative=T)
 for (county_name in county_names) {
   p <- plot_a_county(counties, county_name, "cumulative")
